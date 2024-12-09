332f54bbcc5c2c1c674e457792f81a9ed65ad4a7
ShingHikaruLancers <shing.hikaru@lancers.co.jp>
2024-12-09 17:41:59 +0900

    first commit

---
+++ WebViewMessageApp/WebViewMessageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewMessageAppApp.swift
+//  WebViewMessageApp
+//
+//  Created by shing.hikaru on 2024/12/09.
+//
+
+import SwiftUI
+
+@main
+struct WebViewMessageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
